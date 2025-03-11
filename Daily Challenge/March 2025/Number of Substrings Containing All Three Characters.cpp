ccc848e84eb677bba30a2242ddec3ae0efec6515
sajjat-hossen <sajjat.hossen@brainstation-23.com>
2025-03-11 11:11:21 +0600

    11 Mar, 2025

---
+++ Daily Challenge/March 2025/Number of Substrings Containing All Three Characters.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+  int numberOfSubstrings(string s) {
+    int n = s.size();
+    vector<int> count(3, 0);
+    int left = 0, right = 0;
+    int result = 0;
+
+    while (right < n) {
+      count[s[right] - 'a']++;
+      while (count[0] > 0 && count[1] > 0 && count[2] > 0) {
+        result += n - right;
+        count[s[left] - 'a']--;
+        left++;
+      }
+      right++;
+    }
+    
+    return result;
+  }
+};
